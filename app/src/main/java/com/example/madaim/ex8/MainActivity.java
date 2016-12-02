ab3c4cc55a89b05721f4b309a8d857d33a99bdc7
KaiOrland <kayorland@gmail.com>
2016-12-02 12:04:44 +0200

    dstfhedhgsrgs

--- app/src/main/java/com/example/madaim/ex8/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex8/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.madaim.ex8;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     Button b1;
     EditText ed1;
@@ -17,73 +19,104 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup rGroup;
     double var1;
     double var2;
+    boolean radioCheck;
 
-    public final String ACTION_CHECK = getPackageName() +".check";
-    public final String ACTION_CALC = getPackageName() +".calc";
+    public final static String ACTION_CHECK = "com.example.madaim.ex8.check";
+    public final static String ACTION_CALC = "com.example.madaim.ex8.calc";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = (Button)findViewById(R.id.button);
-        ed1 = (EditText)findViewById(R.id.edFarenheit);
-        ed2 = (EditText)findViewById(R.id.edCelsius);
-        rGroup = (RadioGroup)findViewById(R.id.radioGroup);
-        String StrVar1 = ed1.getText().toString();
-        var1 = Double.parseDouble(StrVar1);
-        if (StrVar1 != null && !StrVar1.equals("")) {
-            var1 = Double.parseDouble(StrVar1);
+        b1 = (Button) findViewById(R.id.button);
+        ed1 = (EditText) findViewById(R.id.edFarenheit);
+        ed2 = (EditText) findViewById(R.id.edCelsius);
+        rGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        ed1.addTextChangedListener(this);
+        ed2.addTextChangedListener(this);
+
+            rGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    ed1.setEnabled(true);
+                    ed2.setEnabled(true);
+
+                    switch (checkedId) {
+                        case R.id.checkRd:
+                            radioCheck = true;
+                            break;
+                        case R.id.calcRd:
+                            radioCheck = false;
+
+                            break;
+                    }
+
+                }
+            });
+            b1.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    String StrVar1 = ed1.getText().toString();
+                    if (StrVar1 != null && !StrVar1.equals("")) {
+                        var1 = Double.parseDouble(StrVar1);
+                    }
+                    String StrVar2 = ed2.getText().toString();
+                    if (StrVar2 != null && !StrVar2.equals(""))
+                        var2 = Double.parseDouble(StrVar2);
+
+                    Intent intent = new Intent(MainActivity.this, Result.class);
+                    if (rGroup.getCheckedRadioButtonId() == R.id.checkRd) {
+                        intent.putExtra("checkFar", var1);
+                        intent.putExtra("checkCel", var2);
+                        intent.setAction(ACTION_CHECK);
+                        startActivity(intent);
+                    } else {
+
+                        if (!ed1.getText().toString().equals("")) {
+                            intent.putExtra("checkFar", var1);
+                            intent.setAction(ACTION_CALC);
+                            startActivity(intent);
+                        } else {
+                            intent.putExtra("checkCel", var2);
+                            intent.setAction(ACTION_CALC);
+                            startActivity(intent);
+                        }
+                    }
+
+                }
+            });
         }
-        String StrVar2 = ed2.getText().toString();
-        if (StrVar1 != null && !StrVar1.equals("")) {
-            var2 = Double.parseDouble(StrVar2);
 
-        rGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(RadioGroup group, int checkedId)
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+        if(ed1.getText().toString().equals("")&&ed2.getText().toString().equals(""))
+            b1.setEnabled(false);
+        else {
+            b1.setEnabled(true);
+            if (radioCheck == false)
             {
-                ed1.setEnabled(true);
-                ed2.setEnabled(true);
-                b1.setEnabled(true);
-                switch(checkedId){
-                    case R.id.checkRd:
-                        break;
-                    case R.id.calcRd:
-                        if(!ed1.getText().toString().equals("")){
-                            ed2.setEnabled(false);
-                        }
-                        if(!ed2.getText().toString().equals("")){
-                            ed1.setEnabled(false);
-                        }
-                        break;
-                }
-
-            }
-        });
-        b1.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Result.class);
-                if (rGroup.getCheckedRadioButtonId() == R.id.checkRd){
-                    intent.putExtra("checkFar", var1 );
-                    intent.putExtra("checkCel", var2 );
-                    intent.setAction(ACTION_CHECK);
-                    startActivity(intent);
-                }
-                else{
-
-                    if(!ed1.getText().toString().equals("")){
-                        intent.putExtra("checkFar", var1 );
-                        intent.setAction(ACTION_CALC);
-                        startActivity(intent);
+                    if (!ed1.getText().toString().equals("")) {
+                        ed2.setEnabled(false);
                     }
-                    else{
-                        intent.putExtra("checkCel", var2 );
-                        intent.setAction(ACTION_CALC);
-                        startActivity(intent);
+                    if (!ed2.getText().toString().equals("")) {
+                        ed1.setEnabled(false);
                     }
                 }
-
+            else {
+                if(!ed1.getText().toString().equals("")&&!ed2.getText().toString().equals(""))
+                    b1.setEnabled(true);
+                else
+                    b1.setEnabled(false);
             }
-        });
+            }
     }
 
 
@@ -112,4 +145,4 @@ public class MainActivity extends AppCompatActivity {
         }
         }
         */
-}
+    }
--- app/src/main/java/com/example/madaim/ex8/Result.java
+++ app/src/main/java/com/example/madaim/ex8/Result.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
@@ -11,18 +12,53 @@ public class Result extends AppCompatActivity {
     double farenheit;
     double celsius;
     TextView tv;
+    String message;
+    boolean isCelsius;
+    Button btnReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        Intent intent = getIntent();
+        btnReturn = (Button)findViewById(R.id.button2);
+        final Intent intent = getIntent();
         Bundle b1 = intent.getExtras();
-        if(b1.containsKey("checkFar"))
-            farenheit = intent.getDoubleExtra("checkFar",0);
-        if(b1.containsKey("checkCel"))
+        if(b1.containsKey("checkFar")) {
+            farenheit = intent.getDoubleExtra("checkFar", 0);
+            isCelsius = false;
+        }
+        if(b1.containsKey("checkCel")) {
             celsius = intent.getDoubleExtra("checkCel", 0);
+            isCelsius = true;
+        }
+        if(intent.getAction().equals(MainActivity.ACTION_CHECK)){
+            if(celsius==(farenheit-32)*(5.0/9))
+                message = getResources().getString(R.string.success, celsius, farenheit);
+            else
+                message = getResources().getString(R.string.fail);
+
+        }
+        else{
+            if(isCelsius==true){
+                farenheit = celsius*(9.0/5)+32;
+                message = getResources().getString(R.string.calculateFar, farenheit, celsius);
+            }
+            else{
+                celsius = (farenheit-32)*(5.0/9);
+                message = getResources().getString(R.string.calculateCel, celsius, farenheit);
+            }
+
+        }
+
 
         tv = (TextView)findViewById(R.id.message);
+        tv.setText(message);
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Result.this, MainActivity.class);
+                startActivity(intent2);
+            }
+            });
     }
 
 
@@ -37,6 +73,6 @@ public class Result extends AppCompatActivity {
         }
 
 
-       //
+
     }
 }
